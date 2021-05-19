26ce770c712c10bbce1569b54962f715dd4a78e9
takerynnaru <74950501+takerynnaru@users.noreply.github.com>
2021-05-19 20:19:23 +0700

    Add Sale off SLIDE
    Thêm slide khuyến mãi cho trang chủ

--- app/src/main/java/com/example/ministop/HomeActivity.java
+++ app/src/main/java/com/example/ministop/HomeActivity.java
@@ -11,7 +11,9 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.widget.FrameLayout;
+import android.widget.ImageView;
 import android.widget.Toast;
+import android.widget.ViewFlipper;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -21,21 +23,23 @@ import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.Volley;
 import com.google.android.material.bottomnavigation.BottomNavigationMenu;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.squareup.picasso.Picasso;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 public class HomeActivity extends AppCompatActivity{
 
-    FrameLayout frameLayout;
-    Fragment currentFragment;
     RecyclerView recyclerView;
     ArrayList<Options> dulieu = new ArrayList<>();
     OptionsAdapter_Recycle optionsAdapter_recycle;
     String url = "http://192.168.1.3/wsministop/getdanhmuc.php";
+
+    ViewFlipper viewFlipper;
 
 
     @Override
@@ -43,12 +47,46 @@ public class HomeActivity extends AppCompatActivity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_trangchu);
 
+        //Ánh xạ
         recyclerView = findViewById(R.id.recycleView_option);
+        viewFlipper = findViewById(R.id.viewflipper);
+        //
 
         optionsAdapter_recycle = new OptionsAdapter_Recycle(this, dulieu);
         recyclerView.setAdapter(optionsAdapter_recycle);
         recyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL,false));
         LayDanhMucSP();
+
+        loadViewFlipper();
+
+    }
+
+     void loadViewFlipper() {
+         String urlslide = "http://192.168.1.3/wsministop/slide/";
+         ArrayList<String> mangslide = new ArrayList<>();
+
+         mangslide.add(urlslide +"1.jpg");
+         mangslide.add(urlslide +"2.jpg");
+         mangslide.add(urlslide +"3.jpg");
+         mangslide.add(urlslide +"4.jpg");
+         mangslide.add(urlslide +"5.jpg");
+         mangslide.add(urlslide +"6.jpg");
+         mangslide.add(urlslide +"7.jpg");
+         mangslide.add(urlslide +"8.jpg");
+         mangslide.add(urlslide +"9.jpg");
+         mangslide.add(urlslide +"10.jpg");
+
+         for(int i = 0; i < mangslide.size(); i++)
+         {
+             ImageView imageView = new ImageView(this);
+             Picasso.with(this).load(mangslide.get(i)).into(imageView);
+             imageView.setScaleType(ImageView.ScaleType.FIT_XY);
+             viewFlipper.addView(imageView);
+         }
+
+         viewFlipper.setAutoStart(true);
+         viewFlipper.setFlipInterval(2000);
+         viewFlipper.startFlipping();
     }
 
     public void LayDanhMucSP()
