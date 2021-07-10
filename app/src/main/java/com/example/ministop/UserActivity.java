277cb9f055a586f320e364f86f0845e14f06143d
phanmainhuy <68746922+phanmainhuy@users.noreply.github.com>
2021-07-10 11:29:36 +0700

    Update OnClick btnSave in UserActivity

--- app/src/main/java/com/example/ministop/UserActivity.java
+++ app/src/main/java/com/example/ministop/UserActivity.java
@@ -2,13 +2,32 @@ package com.example.ministop;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class UserActivity extends AppCompatActivity {
-
+    Button btnSave;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user);
+
+
+        //anh xa
+        btnSave = findViewById(R.id.btnSaveUser);
+
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(UserActivity.this, HomeActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
+
+
+
 }
